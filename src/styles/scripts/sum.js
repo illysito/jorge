62d06e68e37d88db3e3da6fff206ebc8a70f9162
illysito <illyalukyanovdesign@gmail.com>
2025-05-21 09:44:07 +0100

    :)

--- src/styles/scripts/sum.js
+++ src/styles/scripts/sum.js
@@ -1,3 +1,5 @@
+import gsap from 'gsap'
+
 function sum() {
   // elementos del DOM
   const A = document.querySelector('.a')
@@ -6,6 +8,17 @@ function sum() {
   const butt = document.querySelector('.submit_button')
   const stats = document.querySelectorAll('.stats-text')
   const form = document.querySelector('form')
+  const feedbackP = document.querySelector('.feedback-p')
+  const hearts = document.querySelectorAll('.heart')
+  const heartArray = Array.from(hearts)
+
+  // array de feedback correcto
+  const successFeedback = [
+    '¡Súuuuuper!',
+    '¡Bien! El resultado es correcto :)',
+    '¡Genial, sigue así!',
+    '¡Claro que sí!',
+  ]
 
   // prevengo enter de reenviar el formulario
   form.addEventListener('submit', (e) => {
@@ -15,10 +28,13 @@ function sum() {
   // genero 2 números aleatorios para operar
   let prevA = 0
   let prevB = 0
-  console.log(prevB)
   let a = 0
   let b = 0
   let targetResult = a + b
+
+  // errores
+  let difference = 0
+  let addition = 0
 
   // valores de los contadores
   let attemptCount = 0
@@ -26,16 +42,60 @@ function sum() {
   let failCount = 0
   let grade = 0
 
+  // genero la operación
   function generateNumbers() {
     prevA = a
-    console.log('prevA: ' + prevA)
     prevB = b
-    console.log('prevB: ' + prevB)
     a = Math.floor(Math.random() * 31) - 15
-    console.log('a: ' + a)
     b = Math.floor(Math.random() * 31) - 15
-    console.log('b: ' + b)
     targetResult = a + b
+  }
+
+  // defino los posibles errores
+  function errors() {
+    let absA = Math.abs(a)
+    let absB = Math.abs(b)
+    difference = Math.abs(absA - absB)
+    addition = absA + absB
+    console.log(a, b)
+    console.log('diff: ' + difference)
+    console.log('sum: ' + addition)
+    let expectedSign = Math.sign(targetResult)
+    // Si tienen el mismo signo, hay que SUMAR
+    if (targetResult != inputResult) {
+      feedbackP.textContent = 'Mmmmh, parece que te has equivocado...'
+      animateHearts()
+      if (Math.sign(a) == Math.sign(b)) {
+        if (
+          // Si el número introducido corresponde con la suma, pero el signo es diferente del esperado:
+          Math.abs(inputResult) == addition &&
+          Math.sign(inputResult) != expectedSign
+        ) {
+          feedbackP.textContent =
+            '¡Has sumado correctamente los números, pero te has confundido con el signo! El resultado correcto es: ' +
+            targetResult
+        } else if (Math.abs(inputResult) == difference) {
+          feedbackP.textContent =
+            'Ups! Has restado, pero había que sumar. El resultado correcto es: ' +
+            targetResult
+        }
+      } else if (Math.sign(a) != Math.sign(b)) {
+        if (
+          Math.abs(inputResult) == difference &&
+          Math.sign(inputResult) != expectedSign
+        ) {
+          feedbackP.textContent =
+            '¡Has restado correctamente los números, pero te has confundido con el signo! El resultado correcto es: ' +
+            targetResult
+        } else if (Math.abs(inputResult) == addition) {
+          feedbackP.textContent =
+            'Ups! Has sumado, pero había que restar. El resultado correcto es: ' +
+            targetResult
+        }
+      }
+    } else {
+      feedbackP.textContent = successFeedback[Math.floor(Math.random() * 4)]
+    }
   }
 
   // pongo los números en pantalla
@@ -93,18 +153,14 @@ function sum() {
   let inputResult = 0
   function fetchResult() {
     // input
-    if (input.value === '') {
-      console.log('no hay nada!')
-      return
-    }
     inputResult = parseInt(input.value)
 
     // comparacion
     if (inputResult === targetResult) {
-      console.log('yeah! estás en lo cierto :)')
+      // console.log('yeah! estás en lo cierto :)')
       successCount++
     } else {
-      console.log('ups! error :(')
+      // console.log('ups! error :(')
       failCount++
     }
   }
@@ -122,6 +178,7 @@ function sum() {
 
   // reinicio el rollo!
   function restart() {
+    errors()
     generateNumbers()
     printNumbersEffect()
     console.log(targetResult)
@@ -129,24 +186,39 @@ function sum() {
 
   // ejecución del código
   function init() {
+    // errors()
     generateNumbers()
     printNumbers()
     fetchResult()
   }
 
   butt.addEventListener('click', () => {
+    if (input.value == '') {
+      feedbackP.textContent = 'Ups! Debes introducir un valor.'
+      return
+    }
     fetchResult()
     countPerformance()
     restart()
   })
 
   window.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && input.value != '') {
       fetchResult()
       countPerformance()
       restart()
     }
   })
+
+  // animations
+  let currentHeartIndex = heartArray.length - 1
+  function animateHearts() {
+    gsap.to(heartArray[currentHeartIndex], {
+      opacity: 0,
+      duration: 0.8,
+    })
+    currentHeartIndex--
+  }
 
   init()
 }
